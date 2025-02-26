ac4a3e2f520a0d1291a38cb138a5e55c91fb018a
mihir001mv <profecor02@gmail.com>
2025-02-26 14:49:51 +0530

    flood virus.py code

---
+++ flood virus.py
@@ -0,0 +1,14 @@
+'''
+RUN THIS PROGRAM BY OWN RISK,THIS PROGRAM SLOW YOU PC
+'''
+
+print('#Welcome to flood virus.\n')
+
+while True:
+    q = input("*Do you love me?\n a. Yes\n b. No\n c. I don't know\n d. Oh!\n:").lower()  # Convert to lowercase
+    if q in ('a', 'b', 'c', 'd'):
+        while True:
+            print('You got hacked!!\n'*100000000)
+    else:
+        print("Please choose: a, b, c, or d.\n--------------")
+        continue
